3414233579910a4874331c327686241c0c369341
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-07-14 09:56:58 +0500

    main013

--- bin/arithmetic013.dart
+++ bin/arithmetic013.dart
@@ -1,4 +1,6 @@
 // Create function func with arguments
 // the task condition is in the Readme file
-
-void main() {}
+ int func(a,b){
+  return (a*a*a)+3*(a*a)*b+3*a*(b*b)+(b*b*b);
+ }
+void main() {print(func(2,4));}
