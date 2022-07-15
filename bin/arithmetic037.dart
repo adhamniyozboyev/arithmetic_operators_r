898d7955f2d1a3bdc8ceef9e037da6775a097971
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-07-15 10:13:13 +0500

    main037

--- bin/arithmetic037.dart
+++ bin/arithmetic037.dart
@@ -1,4 +1,7 @@
 // Create function func with arguments
 // the task condition is in the Readme file
-
-void main() {}
+import 'dart:math';
+double func(a,b){
+  return 2*(pow(a,b))/(a+b);
+}
+void main() {print(func(2,6));}
